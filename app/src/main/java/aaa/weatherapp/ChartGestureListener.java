dac072fcbc4c29fbf7273f21aeabe87e1cab270d
HerrAachen <6626492+HerrAachen@users.noreply.github.com>
2019-05-22 21:57:27 -0700

    Detect swipe left event

---
+++ app/src/main/java/aaa/weatherapp/ChartGestureListener.java
@@ -0,0 +1,74 @@
+package aaa.weatherapp;
+
+
+import android.util.Log;
+import android.view.MotionEvent;
+
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+
+import java.util.concurrent.Callable;
+
+public class ChartGestureListener implements OnChartGestureListener {
+    private final Runnable swipeLeftCallback;
+    public ChartGestureListener (Runnable swipeLeftCallback) {
+        this.swipeLeftCallback = swipeLeftCallback;
+    }
+    @Override
+    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+        if (isSwipeLeft(me1, me2)) {
+//            Log.i("Event", "Swipe Left");
+            try {
+                swipeLeftCallback.run();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (isSwipeRight(me1, me2)) {
+//            Log.i("Event", "Swipe Right");
+        }
+    }
+
+    private boolean isSwipeLeft(MotionEvent start, MotionEvent end) {
+        return start.getX() - end.getX() > Math.abs(end.getY() - start.getY());
+    }
+
+    private boolean isSwipeRight(MotionEvent start, MotionEvent end) {
+        return end.getX() - start.getX() > Math.abs(end.getY() - start.getY());
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+
+    }
+}
--- app/src/main/java/aaa/weatherapp/MainActivity.java
+++ app/src/main/java/aaa/weatherapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     private WeatherApiClient weatherApiClient;
     private static SimpleDateFormat lastUpdatedDateFormat = new SimpleDateFormat("E hh:mm a");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
@@ -41,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
             showError("An error occurred: " + e.getMessage());
         }
     }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_settings, menu);
@@ -63,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
         configureRainAxis(chartData, chart.getAxisRight());
         configureTemperatureAxis(chartData, chart.getAxisLeft());
         chart.setDescription(null);
+        chart.setOnChartGestureListener(new ChartGestureListener(() -> Log.i("Event", "Swipe left from outside")));
         chart.invalidate();
     }
 
@@ -120,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
     private void showLastUpdatedLabel(ChartData chartData) {
         findViewById(R.id.lastUpdated).setVisibility(View.VISIBLE);
         findViewById(R.id.lastUpdatedDateTime).setVisibility(View.VISIBLE);
-        ((TextView)findViewById(R.id.lastUpdatedDateTime)).setText(lastUpdatedDateFormat.format(chartData.getLastUpdated()));
+        ((TextView) findViewById(R.id.lastUpdatedDateTime)).setText(lastUpdatedDateFormat.format(chartData.getLastUpdated()));
     }
 
     private void showChart(ChartData chartData) {
